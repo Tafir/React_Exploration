01de1b7a84251b3df3dc898a2f518d343663753d
Tafir <vellinarius@yandex.ru>
2020-09-16 18:58:29 +0100

    Started working on implementing sagas

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -8,13 +8,13 @@ import CollectionsOverviewContainer from '../../components/collections-overview/
 import CollectionPageContainer from '../collection/collection.container';
 import '../../components/with-spinner/with-spinner.styles.scss';
 
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
 
 class ShopPage extends React.Component{
     componentDidMount(){
-        const { fetchCollectionsStartAsync} = this.props;
-        fetchCollectionsStartAsync();
+        const { fetchCollectionsStart } = this.props;
+        fetchCollectionsStart();
     }
 
     render(){
@@ -30,7 +30,7 @@ class ShopPage extends React.Component{
 
 
 const mapDispatchToProps = dispatch => ({
-    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+    fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 })
 
 export default connect(null, mapDispatchToProps )(ShopPage);
---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,24 @@
+import { takeEvery, call, put } from 'redux-saga/effects';
+
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+
+import ShopActionTypes from './shop.types';
+import { fetchCollectionsSuccess, fetchCollectionsError } from './shop.actions';
+
+export function* fetchCollectionsStartAsync() {
+    try{
+        const collectionRef = firestore.collection("collections");
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+        yield put(fetchCollectionsSuccess(collectionsMap));
+    } catch (error) {
+        yield put(fetchCollectionsError(error.message));
+    }
+
+}
+
+export function* fetchCollectionsStart() {
+    yield takeEvery(
+        ShopActionTypes.FETCH_COLLECTIONS_START,
+        fetchCollectionsStartAsync);
+}
