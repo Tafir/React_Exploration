fc4c003fbeb5383c460350972a280aac6a174849
Artem Yacenko <vellinarius@yandex.ru>
2020-09-17 15:05:41 +0100

    Cart now clears on log out

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
-import {CartActionTypes} from './cart.types'
-import {addItemToCart, removeItemFromCart} from './cart.utils'
+import { CartActionTypes } from './cart.types'
+import { addItemToCart, removeItemFromCart } from './cart.utils'
 
 
 const INITIAL_STATE = {
@@ -28,6 +28,11 @@ const cartReducer = (state=INITIAL_STATE, action) => {
             return ({
                 ...state,
                 cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
+            })
+        case CartActionTypes.CLEAR_CART:
+            return ({
+                ...state,
+                cartItems: []
             })
         default:
             return state;
--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,4 +1,5 @@
 import { all, call } from 'redux-saga/effects';
+import { cartSagas } from './cart/cart.sagas';
 
 import { fetchCollectionsStart } from './shop/shop.sagas';
 import { userSagas } from './user/user.sagas';
@@ -6,6 +7,7 @@ import { userSagas } from './user/user.sagas';
 export default function* rootSaga() {
     yield all([
         call(fetchCollectionsStart),
-        call(userSagas)
+        call(userSagas),
+        call(cartSagas)
     ])
 }
